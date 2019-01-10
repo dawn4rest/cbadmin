a2f3195a3c9a266710646e70bc9b6179918472f0
dawn4rest <dawn4rest@gmail.com>
2019-01-10 07:58:53 +0000

    feat. Style toggle and slide likes list for mobile

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,21 @@
-$('.slide-trigger').click(function () {
-    $('.slide-body').load('/member/get_liked_posts .slide-body');
+$(document).on('click', '.slide-trigger', function () {
+    $('#slide-menu .inner').load('/member/get_liked_posts .inner');
     $('#slide-menu').addClass('active');
 });
 
-$('.slide-close').click(function () {
+$(document).on('click', '.slide-close', function () {
     $('#slide-menu').removeClass('active');
+    $('body').css('overflow', 'auto');
+});
+
+
+$(document).on('click', '.toggle-trigger', function () {
+    $('#slide-menu .inner').load('/member/toggle');
+    $('#slide-menu').addClass('active');
+    $('body').css('overflow', 'hidden');
+});
+
+$(document).on('click', '.like-trigger', function () {
+    $('#slide-menu .inner').load('/member/get_liked_posts .inner');
+    $('body').css('overflow', 'hidden');
 });
